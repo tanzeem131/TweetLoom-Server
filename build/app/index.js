62bd3660adf5318f3439f44abcc35e5afcce3b2e
tanzeem131 <dev.tanzeem@gmail.com>
2024-07-23 22:34:05 +0530

    implementing google authentication

--- build/app/index.js
+++ build/app/index.js
@@ -17,22 +17,20 @@ const express_1 = __importDefault(require("express"));
 const server_1 = require("@apollo/server");
 const body_parser_1 = __importDefault(require("body-parser"));
 const express4_1 = require("@apollo/server/express4");
+const user_1 = require("./user");
 function initServer() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = (0, express_1.default)();
         app.use(body_parser_1.default.json());
         const graphqlServer = new server_1.ApolloServer({
             typeDefs: `
+            ${user_1.User.types}
             type Query{
-              sayHello: String
-              sayHelloToMe(name:String!):String
+              ${user_1.User.queries}
             }
         `,
             resolvers: {
-                Query: {
-                    sayHello: () => `Hello from graphql server`,
-                    sayHelloToMe: (parent, { name }) => `Hey ${name}`
-                },
+                Query: Object.assign({}, user_1.User.resolvers.queries),
             },
         });
         yield graphqlServer.start();
--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer} from "@apollo/server";
 import bodyParser from "body-parser";
 import { expressMiddleware } from '@apollo/server/express4';
+import { User } from "./user";
 
 export async function initServer() {
     const app = express();
@@ -10,15 +11,14 @@ export async function initServer() {
     
     const graphqlServer = new ApolloServer({
         typeDefs: `
+            ${User.types}
             type Query{
-              sayHello: String
-              sayHelloToMe(name:String!):String
+              ${User.queries}
             }
         `,
         resolvers: {
             Query: {
-                sayHello: () => `Hello from graphql server`,
-                sayHelloToMe: (parent:any, {name}:{name:string})=>`Hey ${name}`
+                ...User.resolvers.queries,
             },
         },
     });
