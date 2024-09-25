cb23237d8ee5cb586071b940cd8f86bcf372b4a2
tanzeem131 <dev.tanzeem@gmail.com>
2024-09-25 23:26:21 +0530

    used aws s3 to store image.

--- src/app/tweet/resolvers.ts
+++ src/app/tweet/resolvers.ts
@@ -1,36 +1,71 @@
 import { Tweet } from "@prisma/client";
 import { prismaClient } from "../../clients/db";
 import { GraphqlContext } from "../../interfaces";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-interface CreateTweetPayload{
-    content:string;
-    imageURL?:string;
+interface CreateTweetPayload {
+  content: string;
+  imageURL?: string;
 }
+const s3Client = new S3Client({
+  region: "",
+  credentials: {
+    accessKeyId: "",
+    secretAccessKey: "",
+  },
+});
 
 const queries = {
-    getAllTweets:()=>
-        prismaClient.tweet.findMany({orderBy:{createdAt:"desc"}}),
+  getAllTweets: () =>
+    prismaClient.tweet.findMany({ orderBy: { createdAt: "desc" } }),
+  getSignedURLForTweet: async (
+    parent: any,
+    { imageType, imageName }: { imageType: string; imageName: string },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user || !ctx.user.id) throw new Error("Unauthenticated");
+    const allowedImageTypes = ["jpg", "jpeg", "png", "webp"];
+
+    if (!allowedImageTypes.includes(imageType))
+      throw new Error("Unsupported Image type");
+
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: "tweetloom-dev",
+      Key: `/uploads/${
+        ctx.user.id
+      }/tweets/${imageName}-${Date.now()}.${imageType}`,
+    });
+
+    const signedURL = await getSignedUrl(s3Client, putObjectCommand);
+
+    return signedURL;
+  },
 };
 
 const mutations = {
-    createTweet: async (parent:any,{payload}:{payload:CreateTweetPayload}, ctx:GraphqlContext)=>{
-        if(!ctx.user) throw new Error ("You are not authenticated");
-        const tweet = await prismaClient.tweet.create({
-            data:{
-                content:payload.content,
-                imageURL:payload.imageURL,
-                author:{connect:{id:ctx.user.id}},
-            },
-        });
-        return tweet;
-    },
+  createTweet: async (
+    parent: any,
+    { payload }: { payload: CreateTweetPayload },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user) throw new Error("You are not authenticated");
+    const tweet = await prismaClient.tweet.create({
+      data: {
+        content: payload.content,
+        imageURL: payload.imageURL,
+        author: { connect: { id: ctx.user.id } },
+      },
+    });
+    return tweet;
+  },
 };
 
 const extraResolvers = {
-    Tweet: {
-        author:(parent:Tweet)=>
-            prismaClient.user.findUnique({where:{id:parent.authorId}}),
-    },
+  Tweet: {
+    author: (parent: Tweet) =>
+      prismaClient.user.findUnique({ where: { id: parent.authorId } }),
+  },
 };
 
-export const resolvers ={mutations,extraResolvers,queries};
+export const resolvers = { mutations, extraResolvers, queries };
